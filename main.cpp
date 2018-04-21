0637a17320fa8afbdbb0c32d836ab484c13cae7d
Keloo <vlad-negura@mail.ru>
2018-04-21 15:38:22 +0300

    adding main.cpp and configure vscode

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _ ios_base::sync_with_stdio(false);cout.precision(30);cout.tie(0);cin.tie(0);
+#define ll long long
+#define ld long double
+#define rep(i, a, b) for(__typeof(b)i=(a)-((a)>(b));i!=(b)-((a)>(b));i+=1-2*((a)>(b)))
+#define whilet() ll t;cin>>t;while(t--)
+#define all(c) (c).begin(), (c).end()
+
+int main() {
+    cout << "Hello" << endl;
+
+    return 0;
+}
